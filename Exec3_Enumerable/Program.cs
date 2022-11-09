19ff186ef5dc9f8640f46763cfd253ea9c57db2a
ispan <ispan@DESKTOP-PAKSETB>
2022-11-09 11:59:30 +0800

    加入專案檔案。

---
+++ Exec3_Enumerable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec3_Enumerable
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var result = Enumerable.Range(1, 10);
+			foreach (var item in result)
+			{
+				Console.WriteLine(item);
+			}
+			var result2=Enumerable.Range(10, 6);
+			foreach (var item in result2)
+			{
+				Console.WriteLine(item);
+			}
+			
+		}
+	}
+}
